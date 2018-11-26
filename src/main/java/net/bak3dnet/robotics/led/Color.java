b9ffe4e927c2f292169d9bea9fb0f629dc1a06bc
Jake Armstrong <Lainkai@users.noreply.github.com>
2018-11-26 10:37:42 -0500

    Base 16 would be nice

--- src/main/java/net/bak3dnet/robotics/led/Color.java
+++ src/main/java/net/bak3dnet/robotics/led/Color.java
@@ -7,6 +7,13 @@ public class Color {
     private byte blue;
 
     public static final Color SoftBlue = new Color("#0086f4");
+    
+    public static final Color BLUE = new Color("00F");
+    public static final Color GREEN = new Color("0F0");
+    public static final Color RED = new Color("F00");
+    public static final Color BLACK = new Color("000");
+    public static final Color WHITE = new Color("FFF");
+    
 
     public static byte[] stringToByteArray(String toRGB) {
 
@@ -20,15 +27,15 @@ public class Color {
 
         if(toRGB.length() == 6) {
 
-            colorArray[0] = (Integer.decode(toRGB.substring(0, 1)).byteValue());
-            colorArray[1] = (Integer.decode(toRGB.substring(2, 3)).byteValue());
-            colorArray[2] = (Integer.decode(toRGB.substring(4, 5)).byteValue());
+            colorArray[0] = (Integer.decode(toRGB.substring(0, 1)).byteValue(), 16);
+            colorArray[1] = (Integer.decode(toRGB.substring(2, 3)).byteValue(), 16);
+            colorArray[2] = (Integer.decode(toRGB.substring(4, 5)).byteValue(), 16);
 
         } else {
 
-            colorArray[0] = (byte) ((Integer.decode(toRGB.substring(0,0)))*17);
-            colorArray[1] = (byte) ((Integer.decode(toRGB.substring(1,1)))*17);
-            colorArray[2] = (byte) ((Integer.decode(toRGB.substring(2,2)))*17);
+            colorArray[0] = (byte) ((Integer.decode(toRGB.substring(0,0)))*17,16);
+            colorArray[1] = (byte) ((Integer.decode(toRGB.substring(1,1)))*17,16);
+            colorArray[2] = (byte) ((Integer.decode(toRGB.substring(2,2)))*17,16);
 
         }      
 
